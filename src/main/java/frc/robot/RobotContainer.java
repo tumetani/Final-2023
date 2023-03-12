37c2d9d347f5f01c0887d96df82a59c946ecee4e
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-03-12 03:21:58 -0400

    implement slowmode?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,19 +7,20 @@ package frc.robot;
 import frc.robot.Constants.ControllerPorts;
 
 //import frc.robot.commands.Autos;
-import frc.robot.commands.OutakeObject;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.DriveTime;
+import frc.robot.commands.ElevatorUp;
 import frc.robot.commands.IntakeObject;
 import frc.robot.commands.LiftArm;
-import frc.robot.commands.LowerArm;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Elevator;
 import frc.robot.Constants.Speeds;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -36,6 +37,8 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Intake m_intake = new Intake();
   private final Arm m_arm = new Arm();
+
+  private final Elevator m_elevator = new Elevator();
   private final DriveTrain m_drive = new DriveTrain();
   // Replace with CommandPS4Controller or CommandJoystick if needed
   public XboxController driverController = new XboxController(ControllerPorts.kDriverControllerPort);
@@ -45,11 +48,27 @@ public class RobotContainer {
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-    m_arm.setDefaultCommand(new LiftArm(m_arm, 0));
-    m_intake.setDefaultCommand(new IntakeObject(m_intake, 0));
+    m_arm.setDefaultCommand(
+        new LiftArm(m_arm, () -> subControllerJoystick.getRawButton(6),
+            () -> subControllerJoystick.getRawButton(4)));
 
+     m_intake.setDefaultCommand(
+      new IntakeObject(m_intake, () -> subControllerJoystick.getRawButton(5),
+        () -> subControllerJoystick.getRawButton(3)));
+
+    m_elevator.setDefaultCommand(
+      new ElevatorUp(m_elevator, () -> subControllerJoystick.getRawAxis(1) * 0.5));
+
+    m_drive.setDefaultCommand(
+      new slowMode(m_drive,() -> driverController.getRawButton(1)));
+
+    configureBindings();
+    m_drive.setDefaultCommand(getarcadeDriveCommand());
+
+    // m_arm.setDefaultCommand(new LiftArm(m_arm, 0));
+
+    // m_intake.setDefaultCommand(new IntakeObject(m_intake, 0));
+    // m_elevator.setDefaultCommand(new ElevatorUp(m_elevator, 0));
   }
 
   private void configureBindings() {
@@ -57,74 +76,29 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is
     // pressed,
     // cancelling on release.
-    // driverController.getRawButton(1).whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
-
-  public void Intake() {
-
-    if (subControllerJoystick.getRawButton(7)) {
-      m_intake.setIntakeSpeed(0.8);
-    } else {
-      m_intake.setIntakeSpeed(0.00);
-    }
-  }
-
-  public void hold() {
-    if (subControllerJoystick.getRawButtonPressed(2)) {
-      m_intake.setIntakeSpeed(0.02);
-    } else {
-      m_intake.setIntakeSpeed(0);
-    }
-  }
-
-  public void Outake() {
-
-    if (subControllerJoystick.getRawButton(3)) {
-      m_intake.setIntakeSpeed(0.8);
-    } else {
-      m_intake.setIntakeSpeed(0.00);
-    }
-  }
-
-  public void Armup() {
-    if (subControllerJoystick.getRawButton(8)) {
-      m_arm.setArmSpeed(-0.67);
-    } else {
-      m_arm.setArmSpeed(0.0);
-    }
-  }
-
-  public void Armdown() {
-    if (subControllerJoystick.getRawButton(5)) {
-      m_arm.setArmSpeed(0.67);
-    } else {
-      m_arm.setArmSpeed(0.00);
-    }
+     //driverController.getRawButton(1).whileTrue(arcadeDrive.());
   }
 
   public Command getAutonomousCommand() {
+    // return new InstantCommand();
     return new SequentialCommandGroup(
-        // new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(1),
-        //new DriveTime(Speeds.DRIVE_SPEED, 2, m_drive),
-        new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(0.5),
-        new ElevatorExtend(m_elevator,Speeds.ELEVATOR_SPEED).withTimeout(1),
-        new OutakeObject(m_intake, 0.80).withTimeout(1),
-        new DriveTime(Speeds.DRIVE_SPEED, 2);
-
-
-
-    // new Outake(m_intake, Constants.INTAKE_SPEED).withTimeout(3),
-    // new LowerArm(m_arm, Speeds.ARM_SPEED).withTimeout(1));
-
+      new DriveTime(Speeds.DRIVE_SPEED, 2, m_drive));
+      //new DriveTrain(Speeds.DRIVE_SPEED, 2, m_drive);
+       // new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(1),
+       // new DriveTrain(Speeds.DRIVE_SPEED, 2, m_drive),
+        //new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(0.5),
+       // new ElevatorUp(m_elevator, Speeds.ELEVATOR_SPEED).withTimeout(0.7),
+        //new IntakeObject(m_intake, -0.80).withTimeout(1),
+        //new DriveTime(Speeds.DRIVE_SPEED, 2, m_drive));
+        //new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(1);
   }
+  public Command getslowModeCommand(){
+return new slowMode(
+  m_drive,() -> driverController.getRawButton(1));}
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  // public Command getAutonomousCommand() {}
-  // An example command will be run in autonomous
+  //m_drive.setDefaultCommand(
+    //new slowMode(m_drive,() -> driverController.getRawButton(1));
+
   public Command getarcadeDriveCommand() {
     return new ArcadeDrive(
         m_drive, () -> -driverController.getRawAxis(1), () -> driverController.getRawAxis(4));
