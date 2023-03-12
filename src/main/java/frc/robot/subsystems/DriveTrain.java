52b0ae68286ed0d1381d313f19eec4c83eeeff51
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-03-12 03:21:04 -0400

    implement slowMode?

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,27 +1,44 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+//import com.ctre.phoenixpro.signals.InvertedValue;
 
+//import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+//import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//import frc.robot.commands.tankDrive;
+//import frc.robot.RobotContainer;
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
-  WPI_TalonSRX leftA = new WPI_TalonSRX(Constants.MotorPorts.LEFT_MOTOR);
-  WPI_TalonSRX leftB = new WPI_TalonSRX(Constants.MotorPorts.LEFT_MOTORA);
-  WPI_TalonSRX rightA = new WPI_TalonSRX(Constants.MotorPorts.RIGHT_MOTOR);
-  WPI_TalonSRX rightB = new WPI_TalonSRX(Constants.MotorPorts.RIGHT_MOTORA);
+  WPI_TalonSRX leftA;
+  WPI_TalonSRX leftB;
+  WPI_TalonSRX rightA;
+  WPI_TalonSRX rightB;
+  MotorControllerGroup leftMotors;
+  MotorControllerGroup rightMotors;
+  DifferentialDrive diffDrive;
 
-  MotorControllerGroup leftMotors = new MotorControllerGroup(leftA, leftB);
-  MotorControllerGroup rightMotors = new MotorControllerGroup(rightA, rightB);
-  DifferentialDrive diffDrive = new DifferentialDrive(leftMotors, rightMotors);
+  // WPI_TalonSRX leftA = new WPI_TalonSRX(Constants.MotorPorts.LEFT_MOTOR);
+  // WPI_TalonSRX leftB = new WPI_TalonSRX(Constants.MotorPorts.LEFT_MOTORA);
+  // WPI_TalonSRX rightA = new WPI_TalonSRX(Constants.MotorPorts.RIGHT_MOTOR);
+  // WPI_TalonSRX rightB = new WPI_TalonSRX(Constants.MotorPorts.RIGHT_MOTORA);
+
+  // MotorControllerGroup leftMotors = new MotorControllerGroup(leftA, leftB);
+  // MotorControllerGroup rightMotors = new MotorControllerGroup(rightA, rightB);
+  // DifferentialDrive diffDrive = new DifferentialDrive(leftMotors, rightMotors);
 
   public DriveTrain() {
+    System.out.println("drivetrain created");
+    leftA = new WPI_TalonSRX(Constants.MotorPorts.LEFT_MOTOR);
+    leftB = new WPI_TalonSRX(Constants.MotorPorts.LEFT_MOTORA);
+    rightA = new WPI_TalonSRX(Constants.MotorPorts.RIGHT_MOTOR);
+    rightB = new WPI_TalonSRX(Constants.MotorPorts.RIGHT_MOTORA);
+    leftMotors = new MotorControllerGroup(leftA, leftB);
+    rightMotors = new MotorControllerGroup(rightA, rightB);
+    diffDrive = new DifferentialDrive(leftMotors, rightMotors);
     diffDrive.setMaxOutput(0.67);
     rightMotors.setInverted(true);
   }
@@ -30,8 +47,14 @@ public class DriveTrain extends SubsystemBase {
     diffDrive.arcadeDrive(forward, turn);
   }
 
+  public void slowMode(double forward, double turn){
+    diffDrive.arcadeDrive(forward*0.2, turn*0.2);
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+
 }
