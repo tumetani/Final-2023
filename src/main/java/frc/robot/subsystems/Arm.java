53a0f39fbec1f156f3bb506778035621c0039bc8
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-03-14 19:41:39 -0400

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,29 +1,30 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
-  CANSparkMax arm_motor = new CANSparkMax(5, MotorType.kBrushless);
+  private final CANSparkMax arm_motor = new CANSparkMax(5, MotorType.kBrushless); // read parameters from Constants.java
 
   public Arm() {
-
   }
 
-  public void setArmSpeed(double speed) {
-    arm_motor.set(-speed);
-  }
-
-  public void LiftArm(double speed) {
-    arm_motor.set(-speed);
-  }
-
-  public void LowerArm(double speed) {
+  // Move
+  public void raise(double speed) {
     arm_motor.set(speed);
+  }
+
+  public void lower(double speed) {
+    arm_motor.set(speed);
+  }
+
+  public void armstop() {
+    arm_motor.set(0);
+  }
+
+  @Override
+  public void periodic() {
   }
 }
