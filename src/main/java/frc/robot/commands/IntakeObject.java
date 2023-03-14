1acec45b5d6c44ba59511cdebca7b7bf419493de
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-03-14 19:40:48 -0400

    Update IntakeObject.java

--- src/main/java/frc/robot/commands/IntakeObject.java
+++ src/main/java/frc/robot/commands/IntakeObject.java
@@ -1,22 +1,39 @@
-// Copyright (c) FIRST and other WPILib contributors.
+/ Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
 
 import frc.robot.subsystems.Intake;
+
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 //import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class IntakeObject extends CommandBase {
+  private final Supplier<Boolean> intake, outake, hold;
+
   private final Intake m_intake;
   double m_speed;
 
-  public IntakeObject(Intake subsystem, double speed) {
+  public IntakeObject(Intake subsystem, Supplier<Boolean> intakeButton, Supplier<Boolean> outakeButton,
+      Supplier<Boolean> holdButton) {
     m_intake = subsystem;
+    intake = intakeButton;
+    outake = outakeButton;
+    hold = holdButton;
     addRequirements(m_intake);
-    m_speed = speed;
   }
+  /*
+   * public IntakeObject(Intake subsystem, Supplier<Boolean> intakeButton,
+   * Supplier<Boolean> outakeButton) {
+   * m_intake = subsystem; orginal!!!
+   * intake = intakeButton;
+   * outake = outakeButton;
+   * addRequirements(m_intake);
+   * }
+   */
 
   // Called when the command is initially scheduled.
   @Override
@@ -26,7 +43,17 @@ public class IntakeObject extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_intake.setIntakeSpeed(0.70);
+
+    if (intake.get()) {
+      m_intake.intake(0.7);
+    } else if (outake.get()) {
+      m_intake.outake(-0.3);
+    } else if (hold.get()) {
+      m_intake.hold(-0.1);
+    } else {
+      m_intake.stopIntake();
+    }
+
   }
 
   // Called once the command ends or is interrupted.
@@ -40,3 +67,4 @@ public class IntakeObject extends CommandBase {
     return false;
   }
 }
+
