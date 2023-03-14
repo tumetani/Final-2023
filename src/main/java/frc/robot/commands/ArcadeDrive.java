12b03d7da3b10baac152e26758fbdac5dfe03edb
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-03-14 19:38:05 -0400

    Update ArcadeDrive.java

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -1,13 +1,8 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
-
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ArcadeDrive extends CommandBase {
   private final DriveTrain m_drive;
@@ -24,13 +19,6 @@ public class ArcadeDrive extends CommandBase {
   @Override
   public void execute() {
     m_drive.arcadeDrive(m_forward.getAsDouble(), m_turn.getAsDouble());
-  }
 
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  public boolean isFinished() {
-    return false;
   }
 }
