a7b47f489bb7d40613fc2e02c1a7df491c607cb1
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-03-14 19:47:11 -0400

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import frc.robot.Constants.ControllerPorts;
 
-//import frc.robot.commands.Autos;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.DriveTime;
 import frc.robot.commands.ElevatorUp;
@@ -20,9 +19,7 @@ import frc.robot.Constants.Speeds;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -37,30 +34,27 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Intake m_intake = new Intake();
   private final Arm m_arm = new Arm();
-
   private final Elevator m_elevator = new Elevator();
   private final DriveTrain m_drive = new DriveTrain();
-  // Replace with CommandPS4Controller or CommandJoystick if needed
+
   public XboxController driverController = new XboxController(ControllerPorts.kDriverControllerPort);
   public Joystick subControllerJoystick = new Joystick(ControllerPorts.kJoystickPort);
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
   public RobotContainer() {
     m_arm.setDefaultCommand(
         new LiftArm(m_arm, () -> subControllerJoystick.getRawButton(6),
             () -> subControllerJoystick.getRawButton(4)));
 
-     m_intake.setDefaultCommand(
-      new IntakeObject(m_intake, () -> subControllerJoystick.getRawButton(5),
-        () -> subControllerJoystick.getRawButton(3)));
+    m_intake.setDefaultCommand(
+        new IntakeObject(m_intake, () -> subControllerJoystick.getRawButton(3),
+            () -> subControllerJoystick.getRawButton(5), () -> subControllerJoystick.getRawButton(2)));
+
+    // m_elevator.setDefaultCommand(
+    // new ElevatorUp(m_elevator, () -> subControllerJoystick.getRawAxis(1) * 0.5));
 
     m_elevator.setDefaultCommand(
-      new ElevatorUp(m_elevator, () -> subControllerJoystick.getRawAxis(1) * 0.5));
-
-    m_drive.setDefaultCommand(
-      new slowMode(m_drive,() -> driverController.getRawButton(1)));
+        new ElevatorUp(m_elevator, () -> subControllerJoystick.getRawButton(10),
+            () -> subControllerJoystick.getRawButton(8)));
 
     configureBindings();
     m_drive.setDefaultCommand(getarcadeDriveCommand());
@@ -76,31 +70,30 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is
     // pressed,
     // cancelling on release.
-     //driverController.getRawButton(1).whileTrue(arcadeDrive.());
+    // driverController.getRawButton(1).whileTrue(arcadeDrive.());
   }
 
   public Command getAutonomousCommand() {
     // return new InstantCommand();
     return new SequentialCommandGroup(
-      new DriveTime(Speeds.DRIVE_SPEED, 2, m_drive));
-      //new DriveTrain(Speeds.DRIVE_SPEED, 2, m_drive);
-       // new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(1),
-       // new DriveTrain(Speeds.DRIVE_SPEED, 2, m_drive),
-        //new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(0.5),
-       // new ElevatorUp(m_elevator, Speeds.ELEVATOR_SPEED).withTimeout(0.7),
-        //new IntakeObject(m_intake, -0.80).withTimeout(1),
-        //new DriveTime(Speeds.DRIVE_SPEED, 2, m_drive));
-        //new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(1);
-  }
-  public Command getslowModeCommand(){
-return new slowMode(
-  m_drive,() -> driverController.getRawButton(1));}
+        new DriveTime(Speeds.DRIVE_SPEED, 3, m_drive));
 
-  //m_drive.setDefaultCommand(
-    //new slowMode(m_drive,() -> driverController.getRawButton(1));
+    // new DriveTrain(Speeds.DRIVE_SPEED, 2, m_drive);
+    // new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(1),
+    // new DriveTrain(Speeds.DRIVE_SPEED, 2, m_drive),
+    // new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(0.5),
+    // new ElevatorUp(m_elevator, Speeds.ELEVATOR_SPEED).withTimeout(0.7),
+    // new IntakeObject(m_intake, -0.80).withTimeout(1),
+    // new DriveTime(Speeds.DRIVE_SPEED, 2, m_drive));
+    // new LiftArm(m_arm, Speeds.ARM_SPEED).withTimeout(1);
+  }
+
+  // m_drive.setDefaultCommand(
+  // new slowMode(m_drive,() -> driverController.getRawButton(1));
 
   public Command getarcadeDriveCommand() {
     return new ArcadeDrive(
-        m_drive, () -> -driverController.getRawAxis(1), () -> driverController.getRawAxis(4));
+        m_drive, () -> -driverController.getRawAxis(1), () -> -driverController.getRawAxis(4),
+        () -> driverController.getRawButton(1)); // change into left bumper?
   }
 }
